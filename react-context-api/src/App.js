372c82ef9c73448bc4532bfd94787a910f984d57
Raj Ratan <110181177+rajratan41@users.noreply.github.com>
2023-05-11 10:34:21 +0000

    added components in App

---
+++ react-context-api/src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Form from "./components/Form";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Form />
+    </>
+  );
+};
+
+export default App;
